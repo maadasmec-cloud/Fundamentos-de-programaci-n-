fe0a7c155253cd8794e846d39dc2bd32fe6402fd
MARCO ANTONIO ADASME CID <ma.adasmec@duocuc.cl>
2026-05-23 20:43:40 -0400

    Update control_temp_horno.py
    puliendo el codigo para que sea continuo y mas optimizado, se agregan ciclos y try except

--- control_temp_horno.py
+++ control_temp_horno.py
@@ -1,36 +1,43 @@
-try:
-    temperatura_alerta = float(input("Ingrese la temperatura de alerta de su horno: "))
-
-    temperatura_advertencia = temperatura_alerta - 5
-
-    
-    registro_de_temperatura = 0
-
-    while True:
-        
-        
-        temperatura_actual = float(input("Ingrese temperatura actual de su horno: "))
-
-        if temperatura_actual <= 0:
-            print("error de registro, se cierra el programa....")
+while True:
+    try:
+        temperatura_alerta = float(input("Ingrese la temperatura de alerta de su horno: "))
+        if temperatura_alerta < 0 or temperatura_alerta > 0:
             break
+    except ValueError:
+        print("Valor debe ser numerico. intente nuevamente")
+
+temperatura_advertencia = temperatura_alerta - 5
 
 
-        elif temperatura_actual > temperatura_advertencia and temperatura_actual < temperatura_alerta:
-            print(f"Cuidado su horno alcanzo {temperatura_actual}° es peligroso ALERTA!!")
+registro_de_temperatura = 0
+
+while True:
+    
+    try:
+        temperatura_actual = float(input("Ingrese temperatura actual de su horno: "))
+        if temperatura_actual <= 0:
+            print("Temperatura debe ser un numero positivo")
+
+        else:
+
+            if temperatura_actual > temperatura_alerta:
+                print(f"Temperatura actual ingresada {temperatura_actual} sobrepasa la temperatura de alerta {temperatura_alerta} PELIGRO!! horno se detiene")
+                break
+    
+            elif temperatura_actual >= temperatura_advertencia:
+                print(f"Cuidado su horno alcanzo {temperatura_actual}° es peligroso ALERTA!!")
 
 
-        elif temperatura_actual > 0 and temperatura_actual < temperatura_alerta:
-            print(f"Temperatura actual es {temperatura_actual}° es registrada")
+            else: 
+                print(f"Temperatura actual es {temperatura_actual}° es registrada")
+            
             
 
-        
-        else:
-            print(f"Su horno alcanzo {temperatura_actual}° Peligro!! horno se detiene")
-            break
+            
+            registro_de_temperatura+= 1
 
-
-        registro_de_temperatura+= 1
+    except ValueError:
+        print("Valor debe ser numerico. intente nuevamente")
 
         
         
@@ -38,7 +45,6 @@ try:
 
 
 
-    print(f"EL total de registros fue {registro_de_temperatura}")
+print(f"EL total de registros fue {registro_de_temperatura}")
 
-except ValueError:
-    print("Valor debe ser numérico")
+
